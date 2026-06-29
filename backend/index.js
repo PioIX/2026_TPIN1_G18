43ced79a53a70b1622df06786fb8d9cf3d57c60d
jlopezdelprado <jlopezdelprado@pioix.edu.ar>
2026-06-29 11:03:56 -0300

    Update index.js

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-const { realizarQuery } = require('./modulos/mysql');
-
-var app = express(); // Creamos la aplicación Express (el servidor)
-var port = process.env.PORT || 4000;
