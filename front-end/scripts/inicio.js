277a87c89f375b6e02bd6cb7447a9359e7a5760d
fvicente-art <fvicente@pioix.edu.ar>
2026-07-02 09:15:07 -0300

    Update inicio.js
    Co-Authored-By: jlopezdelprado <216725509+jlopezdelprado@users.noreply.github.com>

--- front-end/scripts/inicio.js
+++ front-end/scripts/inicio.js
@@ -1,6 +1,17 @@
+if (!localStorage.getItem("usuarioNombre")) {
+  window.location.href = "index.html";
+}
+
+var elementoBienvenida = document.getElementById("bienvenida");
+if (elementoBienvenida) {
+  elementoBienvenida.textContent = "Bienvenido " + localStorage.getItem("usuarioNombre") + "!";
+}
+
 function cerrarSesion() {
-    var confirmado = confirm("Estas seguro de que queres cerrar sesion?");
-    if (confirmado) {
-        window.location.href = "index.html";
-    }
+  var confirmado = confirm("Estas seguro de que queres cerrar sesion?");
+  if (confirmado) {
+    localStorage.removeItem("usuarioNombre");
+    localStorage.removeItem("esAdmin");
+    window.location.href = "index.html";
+  }
 }
